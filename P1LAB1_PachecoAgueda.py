efe6699edc7f6488335b7b0446163a733a1bf969
itschristinaintech <pachecoa6876@student.faytechcc.edu>
2024-06-15 17:34:38 -0400

    First submission

---
+++ P1LAB1_PachecoAgueda.py
@@ -0,0 +1,13 @@
+"""
+ # Agueda Pacheco
+ # June 15, 2024
+ # P1LAB 1
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+"""
+
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
+print(f"Hello {first_name} {last_name}! Welcome to CTI-110")
+
